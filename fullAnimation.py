3725c48755c7d98542a434983e78128ac28e80f6
Mugdha Kurkure <60062557+diamondgelato@users.noreply.github.com>
2021-12-03 10:41:53 +0530

    Modify code for BFS animation.

--- fullAnimation.py
+++ fullAnimation.py
@@ -1,21 +1,18 @@
-# now add color state of each node and edge
-# change color states in breadth first search (BFS)
 import pygame
 import sys
 from graph_data import graph
 
-# constants
 display_width = 1200
 display_height = 900
 radius = 30
-speed = 5 # frames per sec
+speed = 5 
 
-grey = (100, 100, 100)  # undiscovered node or edge
-white = (255, 255, 255)  # discovered edge or node outline
-yellow = (200, 200, 0)  # current node fill
-red = (200,0,0) # discovered node fill
-black = (0, 0, 0)  # undiscovered node fill
-blue = (50,50,160) # completed node fill and completed edge
+grey = (100, 100, 100)
+white = (255, 255, 255)
+yellow = (200, 200, 0) 
+red = (200,0,0)
+black = (0, 0, 0)
+blue = (50,50,160)
 
 # Graph element parts:
 #  [0] : xy 
@@ -26,7 +23,6 @@ blue = (50,50,160) # completed node fill and completed edge
 def run():
     global screen, edges, clock
 
-    # add start colors to graph
     for element in graph:
       element.extend([grey, black])
 
@@ -36,41 +32,40 @@ def run():
 
     screen = pygame.display.set_mode((display_width, display_height))
 
-    draw_graph() # initial
+    draw_graph()
     update()
-    pygame.time.delay(2000) # wait 2 sec to start
-    # BFS algorith loop
-    queue = [0] # start a node, queue of one node
+    pygame.time.delay(2000)
+
+    # BFS algorithm loop
+    queue = [0] 
     while len(queue) > 0:
-        n1 = queue.pop(0) # dequeue node
+        n1 = queue.pop(0) 
         current = graph[n1]   
-        current[2] = white  # current color for node
+        current[2] = white
         current[3] = yellow
         for n2 in current[1]:
-            if graph[n2][3] == black and n2 not in queue:  # undiscoverd
-                queue.append(n2) # enqueue node
-                # discovered n2, color n2 and edge n1,n2
+            if graph[n2][3] == black and n2 not in queue:  
+                queue.append(n2) 
+                
                 graph[n2][2] = white
                 graph[n2][3] = red
                 edges[edge_id(n1,n2)][1] = white
                 update()
-        # mark current as compete
+        
         current[3] = blue
         update()
 
-    while 1:  # wait for stop
+    while 1:  
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.display.quit()
                 sys.exit()
-        # pygame.time.wait(5000) # 5 sec wait
 
-# normalize id for either order
 def edge_id(n1, n2): return tuple(sorted((n1, n2)))  
 
 def build_edges():
     global edges
-    edges = {} # edgeid: [(n1,n2), color]
+    edges = {} 
     for n1, (_, adjacents, _, _) in enumerate(graph):
         for n2 in adjacents:
             eid = edge_id(n1, n2)
@@ -82,11 +77,11 @@ def draw_graph():
 
     screen.fill((0, 0, 0,))
 
-    for e in edges.values(): # draw edges
+    for e in edges.values():
         (n1, n2), color = e
         pygame.draw.line(screen, color, graph[n1][0], graph[n2][0], 2)
 
-    for xy, _, lcolor, fcolor in graph: # draw nodes
+    for xy, _, lcolor, fcolor in graph:
         circle_fill(xy, lcolor, fcolor, 25, 2)
 
 def update():
