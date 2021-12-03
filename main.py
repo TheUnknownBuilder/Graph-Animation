5a694c141d31ce6a8df0781718b6276881036a03
TheUnknownBuilder <vani.kamani@somaiya.edu>
2021-12-03 11:05:22 +0530

    main changes

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import circles
 import addEdges
+import fullAnimation
 
 prompt = """Enter a number:\n
   circles: 1  - basic drawing skills in pygame\n
   add edges: 2 - adding graph edges to drawing
+  full animation: 3 - full animation to the graph
   """
   
 choice = input(prompt)
 
 if choice == '1': circles.run()
 if choice == '2': addEdges.run()
+if choice == '3': fullAnimation.run()
 
 print("run again and type 1, 2, or 3 only")
